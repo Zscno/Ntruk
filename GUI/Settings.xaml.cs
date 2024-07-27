0ef839bf42f4f59bfe0f76caf52191a9a1cd3396
Zscno <zscNofQSpxJHp@outlook.com>
2024-07-27 21:42:09 +0800

    0.9.15.7-Refactoring: Settings page

--- GUI/Settings.xaml.cs
+++ GUI/Settings.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using Ntruk.API;
 using System;
 using System.IO;
+using System.Text;
+using Windows.ApplicationModel.Core;
 using Windows.Storage;
 using Windows.Storage.AccessCache;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using System.Collections.Generic;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -14,102 +16,171 @@ namespace Ntruk.GUI
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class Settings : Page//在之后重构“设置”页面时加入日志系统。
+    public sealed partial class Settings : Page
     {
         public Settings()
         {
             this.InitializeComponent();
         }
 
-        private async void ChangeFolder_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                StorageFolder formerFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken");
-                if (await PickerHelper.UsePickerGetSingleFolder("MinecraftFolderToken") != null)
-                {
-                    StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(Path.Combine((await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken")).Path, "assets", "indexes"));
-                    string[] versionsFile = await MinecraftHelper.GetAllVersions(folder);
-                    StorageFolder versionsFolder = await StorageFolder.GetFolderFromPathAsync(Path.Combine((await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken")).Path, "versions"));
-                    IReadOnlyList<StorageFolder> folders = await versionsFolder.GetFoldersAsync();
-                    string[] versions = new string[folders.Count];
-                    int count = 0;
-                    foreach (var item in folders)
-                    {
-                        foreach (var item1 in versionsFile)
-                        {
-                            if (item.Name.Split('-')[0] == item1)
-                            {
-                                versions[count] = item1;
-                                count++;//没用
-                            }
-                        }
-                    }
-                    currentVersion.PlaceholderText = "请重新选择版本...";
-                    currentFolder.Text = (await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken")).Path;
-                    currentVersion.ItemsSource = versions;
-                }
-                else
-                {
-                    StorageApplicationPermissions.FutureAccessList.AddOrReplace("MinecraftFolderToken", formerFolder);
-                    currentVersion.PlaceholderText = "请重新选择Minecraft文件夹...";
-                }
-            }
-            catch (Exception)
-            {
-                currentTarget.PlaceholderText = "请重新选择文件夹...";
-                ContentDialogHelper.ShowTipDialog("目前选择的Minecarft文件夹不起作用，请再次尝试。");
-            }
-        }
+        private bool useEvent;
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
+            await LogSystem.WriteLog(LogLevel.Info, this, "加载“Settings”界面...");
+            useEvent = false;
+            StorageFolder minecraftFolder;
+            StorageFolder indexesFolder;
+            minecraftFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken");
             try
             {
-                currentFolder.IsReadOnly = true;
-                currentTarget.IsReadOnly = true;
-                currentFolder.Text = (await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken")).Path;
-                currentTarget.Text = (await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("TargetFolderToken")).Path;
-                StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(Path.Combine((await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken")).Path, "assets", "indexes"));
-                string[] versions = await MinecraftHelper.GetAllVersions(folder);
-                string version = string.Empty;
-                foreach (var item in versions)
-                {
-                    if (item == IniHelper.ReadIni("Minecraft", "Version", MainPage.ConfigDataPath))
-                    {
-                        version = item;
-                        break;
-                    }
-                }
-                currentVersion.ItemsSource = versions;
-                currentVersion.SelectedItem = version;
+                indexesFolder = await StorageFolder.GetFolderFromPathAsync(Path.Combine(minecraftFolder.Path, "assets", "indexes"));
             }
             catch (Exception)
             {
-                currentFolder.PlaceholderText = "请重新选择文件夹...";
-                ContentDialogHelper.ShowTipDialog("目前选择的Minecarft文件夹不起作用，请再次尝试。");
+                await LogSystem.WriteLog(LogLevel.Warning, this, $"{Path.Combine(minecraftFolder.Path, "assets", "indexes")}不存在或程序权限不足无法访问。");
+                ContentDialogHelper.ShowTipDialog("我们无法访问所选Minecraft文件夹。它可能不存在或我们无权访问。\n请您更换文件夹再次尝试。");
+                StorageApplicationPermissions.FutureAccessList.Remove("MinecraftFolderToken");
+                useEvent = true;
+                return;
+            }
+            string[] versions = await MinecraftHelper.GetAllVersions(indexesFolder);
+            if (versions.Length == 0)
+            {
+                await LogSystem.WriteLog(LogLevel.Warning, this, $"{indexesFolder.Path}中没有资源索引文件（.json）或无权访问它们。");
+                ContentDialogHelper.ShowTipDialog("我们没有在所选Minecraft文件夹中找到索引文件。它可能不存在或我们无权访问。\n请您更换文件夹或在其中下载一个Minecraft版本以再次尝试。");
+                useEvent = true;
+                return;
+            }
+            currentVersion.ItemsSource = versions;
+            int count = 0;
+            foreach (var item in versions)
+            {
+                if (item == IniHelper.ReadIni("Minecraft", "Version", MainPage.ConfigDataPath))
+                {
+                    break;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            if (count >= versions.Length)
+            {
+                await LogSystem.WriteLog(LogLevel.Warning, this, $"{minecraftFolder.Path}中没有用户上次选择的版本{IniHelper.ReadIni("Minecraft", "Version", MainPage.ConfigDataPath)}（数组索引超出范围）。");
+                ContentDialogHelper.ShowTipDialog("在Minecraft文件夹中我们找不到您选择的版本。\n请您重新选择版本或者文件夹。");
+                currentVersion.PlaceholderText = "重新选择版本...";
+                IniHelper.WriteIni("Minecraft", "Version", string.Empty, MainPage.ConfigDataPath);
+                useEvent = true;
+                goto GetTargetFolder;
+            }
+            currentVersion.SelectedItem = versions[count];
+            currentFolder.Text = minecraftFolder.Path;
+
+            GetTargetFolder:
+            StorageFolder targetFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("TargetFolderToken");
+            if (!Directory.Exists(targetFolder.Path))
+            {
+                await LogSystem.WriteLog(LogLevel.Warning, this, $"{targetFolder.Path}不存在或程序权限不足无法访问。");
+                ContentDialogHelper.ShowTipDialog("我们无法访问所选目标文件夹。它可能不存在或我们无权访问。\n请您更换文件夹再次尝试。");
+                StorageApplicationPermissions.FutureAccessList.Remove("TargetFolderToken");
+                //添加至INI。
+                currentTarget.PlaceholderText = "重新选择文件夹...";
+                useEvent = true;
+                return;
+            }
+            currentTarget.Text = targetFolder.Path;
+            useEvent = true;
+
+            await LogSystem.WriteLog(LogLevel.Info, this, "“Settings”界面加载完成。");
+        }
+
+        private async void ChangeFolder_Click(object sender, RoutedEventArgs e)
+        {
+            await LogSystem.WriteLog(LogLevel.Info, this, "尝试修改Minecraft文件夹...");
+            useEvent = false;
+            changeFolder.IsEnabled = false;
+            StorageFolder formerFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken");
+            if (await PickerHelper.UsePickerGetSingleFolder("MinecraftFolderToken") != null)
+            {
+                StorageFolder minecraftFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("MinecraftFolderToken");
+                StorageFolder indexesFolder;
+                try
+                {
+                    indexesFolder = await StorageFolder.GetFolderFromPathAsync(Path.Combine(minecraftFolder.Path, "assets", "indexes"));
+                }
+                catch (Exception)
+                {
+                    await LogSystem.WriteLog(LogLevel.Warning, this, $"{Path.Combine(minecraftFolder.Path, "assets", "indexes")}不存在或程序权限不足无法访问。");
+                    ContentDialogHelper.ShowTipDialog("我们无法访问所选Minecraft文件夹。它可能不存在或我们无权访问。\n请您更换文件夹再次尝试。");
+                    StorageApplicationPermissions.FutureAccessList.AddOrReplace("MinecraftFolderToken", formerFolder);
+                    changeFolder.IsEnabled = true;
+                    useEvent = true;
+                    return;
+                }
+                string[] versions = await MinecraftHelper.GetAllVersions(indexesFolder);
+                if (versions.Length == 0)
+                {
+                    await LogSystem.WriteLog(LogLevel.Warning, this, $"{indexesFolder.Path}中没有资源索引文件（.json）或无权访问它们。");
+                    ContentDialogHelper.ShowTipDialog("我们没有在所选Minecraft文件夹中找到索引文件。它可能不存在或我们无权访问。\n请您更换文件夹或在其中下载一个Minecraft版本以再次尝试。");
+                    StorageApplicationPermissions.FutureAccessList.AddOrReplace("MinecraftFolderToken", formerFolder);
+                    changeFolder.IsEnabled = true;
+                    useEvent = true;
+                    return;
+                }
+                IniHelper.WriteIni("Minecraft", "Folder", minecraftFolder.Path, MainPage.ConfigDataPath);
+                currentFolder.Text = minecraftFolder.Path;
+                currentVersion.PlaceholderText = "请重新选择版本...";
+                currentVersion.ItemsSource = versions;
+                changeFolder.IsEnabled = true;
+                useEvent = true;
+                await LogSystem.WriteLog(LogLevel.Info, this, "修改Minecraft文件夹成功。");
+            }
+            else
+            {
+                changeFolder.IsEnabled = true;
+                useEvent = true;
+                await LogSystem.WriteLog(LogLevel.Info, this, "用户未选择文件夹，修改Minecraft文件夹失败。");
             }
         }
 
-        private void CurrentVersion_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private async void CurrentVersion_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            IniHelper.WriteIni("Minecraft", "Version", currentVersion.SelectionBoxItem.ToString(), MainPage.ConfigDataPath);
+            if (!useEvent)
+            {
+                return;
+            }
+            await LogSystem.WriteLog(LogLevel.Info, this, "尝试修改Minecraft版本...");
+            if (currentVersion.SelectedItem != null)
+            {
+                IniHelper.WriteIni("Minecraft", "Version", currentVersion.SelectedItem.ToString(), MainPage.ConfigDataPath);
+                await LogSystem.WriteLog(LogLevel.Info, this, "修改Minecraft版本成功。");
+            }
+            else
+            {
+                await LogSystem.WriteLog(LogLevel.Info, this, "用户未选择Minecraft版本，修改Minecraft文件夹失败。");
+            }
         }
 
         private async void ChangeTarget_Click(object sender, RoutedEventArgs e)
         {
-            try
+            await LogSystem.WriteLog(LogLevel.Info, this, "尝试修改目标文件夹...");
+            changeTarget.IsEnabled = false;
+            StorageFolder formerFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("TargetFolderToken");
+            if (await PickerHelper.UsePickerGetSingleFolder("TargetFolderToken") != null)
             {
-                if (await PickerHelper.UsePickerGetSingleFolder("TargetFolderToken") != null)
-                {
-                    currentTarget.Text = (await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("TargetFolderToken")).Path;
-                }
+                changeTarget.IsEnabled = true;
+                StorageFolder targetFolder = await StorageApplicationPermissions.FutureAccessList.GetFolderAsync("TargetFolderToken");
+                currentTarget.Text = targetFolder.Path;
+                await LogSystem.WriteLog(LogLevel.Info, this, "修改目标文件夹成功。");
             }
-            catch (Exception)
+            else
             {
-                currentTarget.PlaceholderText = "请重新选择文件夹...";
-                ContentDialogHelper.ShowTipDialog("目前选择的目标文件夹可能无法使用，请再次尝试。");
+                changeTarget.IsEnabled = true;
+                StorageApplicationPermissions.FutureAccessList.AddOrReplace("TargetFolderToken", formerFolder);
+                await LogSystem.WriteLog(LogLevel.Info, this, "用户未选择文件夹，修改目标文件夹失败。");
             }
         }
+
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,32 @@
-# Ntruk
+﻿# Ntruk
 
-### It is my first complete application.
+[简体中文](#zh-cn)|[English](#en-us)
 
-### If you are interested in this, you can download these source code, and compile these.
+<a id="zh-cn"/>
+
+## 简介
+
+此应用由Zscno编写，为UWP-WinUI2程序，是一个工具箱应用（虽然尚未完成）。目前正在开发阶段，所有功能尚未完工，且并未上架至Microsoft Store。
+
+## 更新：v.0.9.15.7
+
+- 界面`Settings`：
+	- 绑定日志系统；
+	- 修复设置选项修改失败的问题；
+	- 增加运行时检查部分。
+
+## Brief Introduction
+<a id="en-us"/>
+
+> The passage was translated by Baidu Translate. If you think that there are any translation errors, please tell us.
+
+This application is written by Zscno and is a UWP-WinUI2 program. It is a toolbox application (~~although not yet completed~~). Currently in the development phase, all features are not yet completed and have not been released on the Microsoft Store.
+
+## Update: v.0.9.15.7
+
+- `README.md` update.
+
+- Page `Settings`:
+	- Bind the logging system;
+	- Fix the issue of failed modification of settings options;
+	- Add a runtime inspection section.
